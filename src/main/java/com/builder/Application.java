89ecec6078cfa6f2908449988416d06a26c86d82
Nicholas Choga <nicholas.choga@impactradius.com>
2018-12-20 20:04:03 +0200

    application running

---
+++ src/main/java/com/builder/Application.java
@@ -0,0 +1,18 @@
+package com.builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author nicholaschoga
+ * @project CV-Builder
+ **/
+
+@SpringBootApplication
+public class Application {
+        
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
